cfacf34cf4f7de29536288c868b49fe5b2f5d4ce
Ryan Taniguchi <ryan.taniguchi@protonmail.com>
2022-05-02 08:46:15 -0600

    Updated based on feedback

--- lesson2/js/footer.js
+++ lesson2/js/footer.js
@@ -1,4 +1,4 @@
 // Determines current year and the date when the file was last modified so they can be placed in the footer.
 const options = {year: "numeric"}
 const currentyear = new Date().toLocaleDateString("en-US", options);
-document.querySelector("#footertext").innerHTML = `&copy; ${currentyear} ║ Ryan Taniguchi ║ Last modified: ${document.lastModified}`;
+document.querySelector("#footertext").innerHTML = `&copy; ${currentyear} ║ Ryan Taniguchi ✧ ║ Last modified: ${document.lastModified}`;
