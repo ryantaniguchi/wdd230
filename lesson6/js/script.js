df7424fe561558bc6ee77e7d6259f87bffb69f3c
Ryan Taniguchi <ryan.taniguchi@protonmail.com>
2022-05-24 20:45:06 -0600

    W06 Rough Draft

---
+++ lesson6/js/script.js
@@ -0,0 +1,26 @@
+// Hamburger menu
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+
+// Date functions
+const datefield = document.querySelector(".date");
+const now = new Date();
+const currentdate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+// Current date
+datefield.innerHTML = `${currentdate}`;
+
+let meetandgreet = document.querySelector('#meetandgreet');
+
+let day = now.getDay();
+
+if(day == 1 || day == 2){
+    meetandgreet.style.display = "block";
+}
+
+
+// Footer content
+const options = {year: "numeric"}
+const currentyear = now.toLocaleDateString("en-US", options);
+document.querySelector("#footertext").innerHTML = `&copy;${currentyear} Black Forest Chamber ║ Ryan Taniguchi ✧ ║ WDD 230 Project ║ Last modification: ${document.lastModified}`;
