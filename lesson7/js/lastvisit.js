c21a0482e0b5646f8855b2b647a03bd86b8f00ec
Ryan Taniguchi <ryan.taniguchi@protonmail.com>
2022-06-02 07:04:05 -0600

    W07 Update

--- lesson7/js/lastvisit.js
+++ lesson7/js/lastvisit.js
@@ -2,7 +2,8 @@
 let elapsed = document.querySelector("#lastVisit");
 
 // get the stored value in localStorage
-const lastvisit = Number(localStorage.getItem("lastvisit"));
+let lastvisit = Number(window.localStorage.getItem("lastvisit"));
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
 
 // Calculate time between visits
 const currentvisit = Date.now() - lastvisit;
@@ -11,7 +12,15 @@ const currentvisit = Date.now() - lastvisit;
 const daysbetween = currentvisit / (1000 * 60 *60 * 24);
 
 // Create string update to be returned
-elapsed.textContent = `Days since your last visit: ${Math.round(daysbetween)}`;
+if (numVisits !== 0) {
+    elapsed.textContent = `Days since your last visit: ${Math.round(daysbetween)}`;
+} else {
+    elapsed.textContent = `Welcome to Black Forest. This is your first visit.`;
+}
 
-// store the new number of visits value
+// increment the number of visits.
+numVisits++;
+
+// store the lastvisit and visits-ls data.
 localStorage.setItem("lastvisit", Date.now());
+localStorage.setItem("visits-ls", numVisits);
