8e9299177cb9e743f4f6163d20ff83bcd1e0854a
Ryan Taniguchi <ryan.taniguchi@protonmail.com>
2022-06-21 15:06:26 -0600

    Weather API rough draft

---
+++ chamber/js/weatherapi.js
@@ -0,0 +1,36 @@
+// select HTML elements to edit
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const windSpeed = document.querySelector('#speed');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/forecast?q=denver&units=imperial&appid=3af250634d071cb3f84c2c5c48e9d30a";
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // this is for testing the call
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
+
+apiFetch();
+
+function  displayResults(weatherData) {
+  currentTemp.innerHTML = `<strong>${weatherData.list[0].main.temp.toFixed(0)}</strong>`;
+  windSpeed.innerHTML = `${weatherData.list[0].wind.speed}`;
+
+  const iconsrc = `https://openweathermap.org/img/w/${weatherData.list[0].weather[0].icon}.png`;
+  const desc = weatherData.list[0].weather[0].description;
+
+  weatherIcon.setAttribute('src', iconsrc);
+  weatherIcon.setAttribute('alt', desc);
+  captionDesc.textContent = desc;
+}
