c87ee31652dce1567c955fca75db27cb3d8d8d68
Ryan Taniguchi <ryan.taniguchi@protonmail.com>
2022-07-01 14:18:10 -0600

    W11 final

--- chamber/js/script.js
+++ chamber/js/script.js
@@ -6,7 +6,9 @@ function toggleMenu() {
 // Date functions
 const datefield = document.querySelector(".date");
 const now = new Date();
-const currentdate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+const currentdate = new Intl.DateTimeFormat("en-US", {
+    dateStyle: "full"
+}).format(now);
 
 // Current date
 datefield.innerHTML = `${currentdate}`;
@@ -15,18 +17,21 @@ let meetandgreet = document.querySelector('#meetandgreet');
 
 let day = now.getDay();
 
-if(day == 1 || day == 2){
+if (day == 1 || day == 2) {
     meetandgreet.style.display = "block";
 }
 
 const dismiss = document.querySelector('.dismiss');
 dismiss.addEventListener('click', closeFunction);
+
 function closeFunction() {
-    this.parentElement.style.display='none';
+    this.parentElement.style.display = 'none';
 }
 
 
 // Footer content
-const options = {year: "numeric"}
+const options = {
+    year: "numeric"
+}
 const currentyear = now.toLocaleDateString("en-US", options);
 document.querySelector("#footertext").innerHTML = `&copy;${currentyear} Black Forest Chamber ║ Ryan Taniguchi ✧ ║ WDD 230 Project ║ Last modification: ${document.lastModified}`;
